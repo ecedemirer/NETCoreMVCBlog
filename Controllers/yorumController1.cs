768b1f916a08695bd56e088f9ca5c348414eeff0
Ece Demirer <edemirer850@gmail.com>
2023-07-15 09:36:45 +0300

    yorum değişikliği

---
+++ Controllers/yorumController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NETCoreMVCBlog.Controllers
+{
+    public class yorumController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
